20f896041d9b4d1d4d3d19af779509e3d55e9904
Harry Lees <harry.lees@gmail.com>
2021-06-03 18:13:27 +0100

    added MutableMapping methods

--- commands.py
+++ commands.py
@@ -1,19 +1,34 @@
-class Commands:
+from collections.abc import MutableMapping
+from typing import Dict, Callable, Iterator
+
+class Commands(MutableMapping):
     def __init__(self) -> None:
-        self.commands = {
-            'help' : self.help
-        }
+        self.commands: Dict[str, Callable] = {'help' : self.help}
 
     def __repr__(self) -> str:
         return f'Commands(' + ', '.join(cmd for cmd in self.commands) + ')'
 
-    def register(self, cmd, callback):
-        if cmd in self.commands:
-            raise ValueError('a function has already been registed to this command')
+    def __getitem__(self, cmd) -> Callable:
+        try:
+            return self.commands[cmd]
+        except KeyError:
+            raise KeyError(f'command not found: {cmd}')
 
+    def __setitem__(self, cmd: str, callback: Callable) -> None:
         self.commands[cmd] = callback
 
-    def execute(self, cmd, *args):
+    def __delitem__(self, cmd) -> None:
+        del self.commands[cmd]
+
+    def __iter__(self) -> Iterator:
+        return iter(self.commands)
+
+    def __len__(self) -> int:
+        return len(self.commands)
+
+    def execute(self, cmd: str, *args) -> None:
+        '''Execute the command from the CLI'''
+
         command = self.commands.get(cmd)
         if command:
             try:
@@ -23,7 +38,7 @@ class Commands:
         else:
             raise ValueError('there is no command with that name')
 
-    def help(self):
+    def help(self) -> None:
         '''Prints the help for all registered functions'''
 
         for cmd in self.commands.values():
@@ -31,5 +46,7 @@ class Commands:
             print('  ', cmd.__doc__)
 
     @property
-    def completer(self):
+    def completer(self) -> tuple:
+        '''returns the commands which can be used for autocomplete'''
+
         return tuple(self.commands.keys())
